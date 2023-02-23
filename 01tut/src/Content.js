6812d3fb20ccf993f2d80386ff55cf437685047a
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-23 17:50:09 +0000

    started chapter 7

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,39 +1,36 @@
 import React from 'react'
+import {FaTrashAlt} from 'react-icons/fa';
 import {useState} from 'react';
 export default function Content() {
-    const [name,setName] = useState();
-    let [count,setCount]=useState(0);
-    const handleNameChange = () =>{
-        const names = ["Bob","Kevin","George","Katie","Mike"];
-        const int = Math.floor(Math.random()*(names.length))
-        setName(names[int]);
-      }
-      const handleClick = () =>{
-        console.log('You clicked');
-        handleNameChange()
-      }
-      const handleClick2 = (name) =>{
-        console.log(`${name} was clicked`);
-      }
-      const handleClick3 = (e) =>{
-        console.log(e.target);
-      }
-      //This takes in "e" the event handler
-      const raise = () =>{
-        count = count+1;
-        setCount(count);
-      }
+    const [items,setItems] = useState([{
+      id:1,
+      checked:false,
+      item:"One half pound bag of cocoa"
+    },
+  {
+    id:2,
+    checked:false,
+    item:"Item2"
+  },
+{
+  id:3,
+  checked:false,
+  item:"item 3"
+}]);
+    
   return (
     //The ondoubleClick is used for clicking on the text and not buttons
     <main>
-      
-        <p onDoubleClick={handleClick}>Hello {name}</p>
-        <button onClick={handleClick}>Click to change</button>
-        <button onClick={()=>handleClick2('Dave')}>Click</button>
-        <button onClick={(e)=>handleClick3(e)}>Click MEEE</button>
-        <button onClick={raise}>Raise the number</button>
-        <hr></hr>
-        <p>{count}</p>
+      <ul>
+        {items.map((item)=>(
+          //Each list item needs a key, helps React identify changes
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked}/>
+            <label>{item.item}</label>
+            <FaTrashAlt role="button" tabIndex="0"/>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
