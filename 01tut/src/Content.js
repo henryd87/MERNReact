9e6525b2a11ad0c0f96aea815d2e24264b96b9a5
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-23 03:03:39 +0000

    finished chapter 6

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import {useState} from 'react';
 export default function Content() {
+    const [name,setName] = useState();
+    let [count,setCount]=useState(0);
     const handleNameChange = () =>{
-        const names = ["Bob","Kevin"];
+        const names = ["Bob","Kevin","George","Katie","Mike"];
         const int = Math.floor(Math.random()*(names.length))
-        return names[int];
+        setName(names[int]);
       }
       const handleClick = () =>{
         console.log('You clicked');
@@ -17,14 +19,21 @@ export default function Content() {
         console.log(e.target);
       }
       //This takes in "e" the event handler
+      const raise = () =>{
+        count = count+1;
+        setCount(count);
+      }
   return (
     //The ondoubleClick is used for clicking on the text and not buttons
     <main>
       
-        <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
-        <button onClick={handleClick}>Hello {handleNameChange()}</button>
+        <p onDoubleClick={handleClick}>Hello {name}</p>
+        <button onClick={handleClick}>Click to change</button>
         <button onClick={()=>handleClick2('Dave')}>Click</button>
         <button onClick={(e)=>handleClick3(e)}>Click MEEE</button>
+        <button onClick={raise}>Raise the number</button>
+        <hr></hr>
+        <p>{count}</p>
     </main>
   )
 }
