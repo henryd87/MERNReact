78c16e91b709cacba5a8ee34875d5b6a0e548796
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-23 18:15:56 +0000

    finished chapter 7, able to udnerstand more about state

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -17,20 +17,36 @@ export default function Content() {
   checked:false,
   item:"item 3"
 }]);
+  const handleCheck = (id) =>{
+    const listItems = items.map(
+      (item)=>item.id===id ? {...item,checked:!item.checked} : item);
+      setItems(listItems);
+      localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+  }
+  const handleDelete = (id) =>{
+    const listItems = items.filter((item)=>item.id!==id);
+    setItems(listItems);
+      localStorage.setItem('shoppinglist',JSON.stringify(listItems));
+  }
     
   return (
     //The ondoubleClick is used for clicking on the text and not buttons
     <main>
+      {items.length ? (
       <ul>
         {items.map((item)=>(
           //Each list item needs a key, helps React identify changes
           <li className="item" key={item.id}>
-            <input type="checkbox" checked={item.checked}/>
-            <label>{item.item}</label>
-            <FaTrashAlt role="button" tabIndex="0"/>
+            <input type="checkbox" onChange={()=> handleCheck(item.id)} checked={item.checked}/>
+            <label
+            style={(item.checked) ? {textDecoration:'line-through'}:null}
+            onDoubleClick = {()=>handleCheck(item.id)}
+            >{item.item}
+            </label>
+            <FaTrashAlt role="button" onClick={()=>handleDelete(item.id)} tabIndex="0"/>
           </li>
         ))}
-      </ul>
+      </ul> ) : (<p style = {{marginTop:'2rem'}}>Your list is empty</p>)}
     </main>
   )
 }
