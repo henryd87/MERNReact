92c843e725a1365f0936c933ecf8bfa358cb815d
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-23 00:36:28 +0000

    miscellaneous react stuff

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -6,9 +6,25 @@ export default function Content() {
         const int = Math.floor(Math.random()*(names.length))
         return names[int];
       }
+      const handleClick = () =>{
+        console.log('You clicked');
+        handleNameChange()
+      }
+      const handleClick2 = (name) =>{
+        console.log(`${name} was clicked`);
+      }
+      const handleClick3 = (e) =>{
+        console.log(e.target);
+      }
+      //This takes in "e" the event handler
   return (
+    //The ondoubleClick is used for clicking on the text and not buttons
     <main>
-        <p>{handleNameChange()}</p>
+      
+        <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+        <button onClick={handleClick}>Hello {handleNameChange()}</button>
+        <button onClick={()=>handleClick2('Dave')}>Click</button>
+        <button onClick={(e)=>handleClick3(e)}>Click MEEE</button>
     </main>
   )
 }
--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,4 +1,5 @@
 const Header = () =>{
+   
     return(
         <header>
             <h1>Groceries List</h1>
